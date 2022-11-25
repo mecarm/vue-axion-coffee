f37098f1fec0d2fcbf7a147f875c4cb504156a21
mecarm <melecarmine99@gmail.com>
2022-11-25 18:23:58 +0100

    importo con axios api e stampo a schermo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <MainComp/>
+    <div class="logo">
+      <img src="https://i.pinimg.com/originals/fa/16/ec/fa16ec19e6212b6c68daeea1b06f4a52.png" alt="">
+    </div>
+    <div class="cards">
+      <MainComp v-for="(element, index) in dataCoffee" :key="index" :dettaggliCoffe="element"/>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import MainComp from './components/MainComp.vue'
 
 export default {
   name: 'App',
   components: {
     MainComp
+  },
+  data() {
+    return{
+      dataCoffee: ''
+    }
+  },
+  mounted(){
+    axios.get('https://api.sampleapis.com/coffee/hot')
+      .then ( (response) =>{
+        this.dataCoffee = response.data
+      })
   }
 }
 </script>
@@ -24,5 +40,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .logo{
+    width: 200px;
+  }
+  img{
+    width: 100%;
+  }
+}
+.cards{
+  width: 80%;
+  margin: 20px auto;
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
-
+  <div class="cardse">
+    <img :src="dettaggliCoffe.image" alt="">
+    <h1>{{dettaggliCoffe.title}}</h1>
+    <p>{{dettaggliCoffe.description}}</p>
+    <div>{{dettaggliCoffe.ingredients}}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MainComp',
-  props: {
-
+  props: {  
+    dettaggliCoffe: Object
   }
 }
 </script>
 
 <style scoped lang="scss">
+.cardse{
+  width: calc((100% / 4) - 40px);
+  margin: 20px;
 
+  p, div{
+    text-align: start;
+  }
+
+  img{
+    width: 100%;
+    aspect-ratio: 1/1;
+  }
+}
 </style>
