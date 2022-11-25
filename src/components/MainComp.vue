74094f5577830a065d51fd600ed36a58438e8244
mecarm <melecarmine99@gmail.com>
2022-11-25 17:45:56 +0100

    Cre file lavoro vue

---
+++ src/components/MainComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+//importo Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
